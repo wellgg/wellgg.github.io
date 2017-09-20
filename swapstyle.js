f871e8c1f81729e60873167655ff98ca3123c058
NitroGuy10 <nitroguy@tx.rr.com>
2017-09-20 09:12:10 -0500

    Forgive my ugly code!

--- swapstyle.js
+++ swapstyle.js
@@ -1,1 +1,10 @@
-a
+function setStyle () {
+
+if (screen.width <= 800) {
+
+document.getElementById("sheet").setAttribute("href", "mstyle.css");  
+
+}else{
+document.getElementById("sheet").setAttribute("href", "style.css");  
+}
+}
